70dcd2c173e8ce09e9a532438aac345eb52ade9a
LuK1337 <priv.luk@gmail.com>
2024-11-03 11:05:48 +0100

    a71: Switch to py extract utils
    Change-Id: I8870e45e429fd651892f2da1bfc2452ba4fb545a

---
+++ extract-files.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_lib import (
+    lib_fixup_vendorcompat,
+    lib_fixups_user_type,
+    libs_proto_3_9_1,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+namespace_imports = [
+    'vendor/samsung/a71-common',
+]
+
+lib_fixups: lib_fixups_user_type = {
+    libs_proto_3_9_1: lib_fixup_vendorcompat,
+}
+
+module = ExtractUtilsModule(
+    'a71',
+    'samsung',
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    check_elf=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device_with_common(
+        module, 'a71-common', module.vendor
+    )
+    utils.run()
